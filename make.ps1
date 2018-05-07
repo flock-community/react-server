0368385adb1268a908d2a6f3c52eae74d2064e66
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2018-05-07 23:32:44 +0200

    Custom webpack config

--- make.ps1
+++ make.ps1
@@ -1,11 +1,11 @@
 param (
     [parameter(Position=0)]
-    [ValidateSet('s', 'f')]
-    [String]$stackOrFrontend=$(throw "Full stack (s) or Frontend (f)? Example: .\make.ps1 s b"),
+    [ValidateSet('stack', 'fe')]
+    [String]$stackOrFrontend=$(throw "Full stack (stack) or Frontend (fe)? Example: .\make.ps1 stack build"),
 
     [parameter(Position=1)]
-    [ValidateSet('b', 'r', 'y')]
-    [String]$buildOrRun=$(throw "Build (b) or Run (r)? Example: .\make.ps1 s r")
+    [ValidateSet('build', 'run')]
+    [String]$buildOrRun=$(throw "Build (build) or Run (run)? Example: .\make.ps1 stack run")
 )
 
 $currentDir = Split-Path $MyInvocation.MyCommand.Path
@@ -15,16 +15,16 @@ $buildFrontend = 'docker build -f Dockerfile-fe -t react-server-fe .'
 $runStack      = 'docker run -p 8080:8080 --name stack --rm react-server-full'
 $runFrontend   = 'docker run -p 4000:4000 -it --name fe --rm -v ' + $currentDir + '\src\main\frontend:/app react-server-fe bash'
 
-if ($buildOrRun -eq 'b' -And $stackOrFrontend -eq 's') {
+if ($buildOrRun -eq 'build' -And $stackOrFrontend -eq 'stack') {
     Write-Output $buildStack
     Invoke-Expression $buildStack
-} elseif ($buildOrRun -eq 'b' -And $stackOrFrontend -eq 'f') {
+} elseif ($buildOrRun -eq 'build' -And $stackOrFrontend -eq 'fe') {
     Write-Output $buildFrontend
     Invoke-Expression $buildFrontend
-} elseif ($buildOrRun -eq 'r' -And $stackOrFrontend -eq 's') {
+} elseif ($buildOrRun -eq 'run' -And $stackOrFrontend -eq 'stack') {
     Write-Output $runStack
     Invoke-Expression $runStack
-} elseif ($buildOrRun -eq 'r' -And $stackOrFrontend -eq 'f') {
+} elseif ($buildOrRun -eq 'run' -And $stackOrFrontend -eq 'fe') {
     Write-Output $runFrontend
     Invoke-Expression $runFrontend
 } else {
